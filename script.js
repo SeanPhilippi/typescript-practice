e3ffde0f6ecfb5ff6e15b29369cbd47d62cda7d5
Sean Philippi <sean.philippi@gmail.com>
2019-08-25 18:45:12 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+// string
+var myName = 'Sean';
+// myName = 8;
+// number
+var myAge = 29;
+// myAge = 'Sean';
+// boolean
+var hasHobbies = false;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 30;
+myRealAge = '30';
+// array
+var hobbies = ['film', 'music', 'studying'];
+hobbies = [100];
+hobbies = 100;
---
+++ script.ts
@@ -0,0 +1,35 @@
+// string
+let myName = 'Sean';
+// myName = 8;
+
+// number
+let myAge = 29;
+// myAge = 'Sean';
+
+// boolean
+let hasHobbies = false;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 30;
+// myRealAge = '30';
+
+// array
+let hobbies: any[] = ['film', 'music', 'studying'];
+hobbies = [100];
+// hobbies = 100;
+hobbies = [true, false];
+
+// tuples
+let address: [string, number] = ["superstreet", 99];
+
+// enums
+enum Color {
+  Gray,
+  Green,
+  Blue
+}
+
+let myColor: Color = Color.Green;
+
