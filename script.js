274c1d4e5d7443ac752a96033c18213f7607140a
Sean Philippi <sean.philippi@gmail.com>
2019-08-25 20:07:04 -0500

    function experimentation

--- script.js
+++ script.js
@@ -11,8 +11,50 @@ var hasHobbies = false;
 // assign types
 var myRealAge;
 myRealAge = 30;
-myRealAge = '30';
+// myRealAge = '30';
 // array
 var hobbies = ['film', 'music', 'studying'];
 hobbies = [100];
-hobbies = 100;
+// hobbies = 100;
+hobbies = [true, false];
+// tuples
+var address = ["superstreet", 99];
+// enums
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var myColor = Color.Green;
+console.log('myColor', myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(3);
+// function
+// type refers to return value of function
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// void
+// throw error if function returns anything
+function sayHello() {
+    console.log("Hello!");
+    return myName;
+}
+// argument types
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+// console.log(multiply(2, 'Max'));
+console.log(multiply(10, 2));
+// function types
+var myMultiply;
+myMultiply = sayHello;
+console.log(myMultiply());
+myMultiply = multiply;
+console.log(myMultiply(3, 3));
--- script.ts
+++ script.ts
@@ -29,7 +29,41 @@ enum Color {
   Gray,
   Green,
   Blue
-}
+};
 
 let myColor: Color = Color.Green;
+console.log('myColor', myColor);
 
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(3);
+
+// function
+// type refers to return value of function
+function returnMyName(): string {
+  return myName;
+}
+console.log(returnMyName());
+
+// void
+// throw error if function returns anything
+function sayHello(): void {
+  console.log("Hello!");
+  // return myName;
+}
+
+// argument types
+function multiply(val1: number, val2: number): number {
+  return val1 * val2;
+}
+// console.log(multiply(2, 'Max'));
+console.log(multiply(10, 2));
+
+// function types
+let myMultiply;
+myMultiply = sayHello;
+console.log(myMultiply())
+myMultiply = multiply;
+console.log(myMultiply(3, 3));
